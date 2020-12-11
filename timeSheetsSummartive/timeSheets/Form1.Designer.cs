9e71e34c717336b3dbdb642ebd30a61a30b2fffe
Mbali Ntombela <mbalisizkele7@gmail.com>
2020-12-11 20:49:25 +0200

    summartive

---
+++ timeSheetsSummartive/timeSheets/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace timeSheets
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultTxt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.usersTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.GetUsers = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.getTimeslots = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.delete = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please Choose Month";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July ",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.comboBox1.Location = new System.Drawing.Point(129, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Please Choose Year";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "2016",
+            "2017",
+            "2018",
+            "2019"});
+            this.comboBox2.Location = new System.Drawing.Point(129, 86);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(175, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "GetHoursPerPeson";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // resultTxt
+            // 
+            this.resultTxt.Location = new System.Drawing.Point(12, 186);
+            this.resultTxt.Multiline = true;
+            this.resultTxt.Name = "resultTxt";
+            this.resultTxt.Size = new System.Drawing.Size(767, 107);
+            this.resultTxt.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(310, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "GetHoursPerProj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // usersTxt
+            // 
+            this.usersTxt.Location = new System.Drawing.Point(487, 50);
+            this.usersTxt.Name = "usersTxt";
+            this.usersTxt.Size = new System.Drawing.Size(100, 20);
+            this.usersTxt.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(490, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Please Enter String Below";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // GetUsers
+            // 
+            this.GetUsers.Location = new System.Drawing.Point(493, 81);
+            this.GetUsers.Name = "GetUsers";
+            this.GetUsers.Size = new System.Drawing.Size(75, 23);
+            this.GetUsers.TabIndex = 10;
+            this.GetUsers.Text = "Get Users";
+            this.GetUsers.UseVisualStyleBackColor = true;
+            this.GetUsers.Click += new System.EventHandler(this.GetUsers_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(677, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Please Enter Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(677, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Please Choose Month";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(670, 39);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(110, 20);
+            this.nameTxt.TabIndex = 13;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July ",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.comboBox3.Location = new System.Drawing.Point(670, 81);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 14;
+            // 
+            // getTimeslots
+            // 
+            this.getTimeslots.Location = new System.Drawing.Point(680, 157);
+            this.getTimeslots.Name = "getTimeslots";
+            this.getTimeslots.Size = new System.Drawing.Size(75, 23);
+            this.getTimeslots.TabIndex = 15;
+            this.getTimeslots.Text = "GetTimeslots";
+            this.getTimeslots.UseVisualStyleBackColor = true;
+            this.getTimeslots.Click += new System.EventHandler(this.getTimeslots_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(677, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Please Choose Year";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "2016",
+            "2017",
+            "2018",
+            "2019"});
+            this.comboBox4.Location = new System.Drawing.Point(670, 121);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 18;
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(0, 157);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 19;
+            this.delete.Text = "delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.getTimeslots);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.nameTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.GetUsers);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.usersTxt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.resultTxt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox resultTxt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox usersTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button GetUsers;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nameTxt;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button getTimeslots;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button delete;
+    }
+}
+
---
+++ timeSheetsSummartive/timeSheets/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace timeSheets
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private SqlConnection CreateConnection()
+        {
+            return new SqlConnection("Server = localhost\\SQLEXPRESS;Initial Catalog=Timesheets;Integrated Security=True");
+        }
+        private void updateoutput(IEnumerable<object> lists)
+        {
+            resultTxt.Clear();
+            foreach (var list in lists) 
+               
+
+            {
+                resultTxt.Text += list;
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var months = comboBox1.SelectedItem.ToString();
+            var year = comboBox2.SelectedItem.ToString();
+
+
+            using (var conn = CreateConnection())
+            {
+
+                var getHours = conn.Query<Users>(@"SELECT u.Username, t.TotalHoursCaptured FROM Users AS u
+                                                    JOIN (SELECT UserId,SUM(HoursCaptured) AS TotalHoursCaptured FROM Timeslots
+                                                    WHERE DATENAME(month, Date)=@month AND YEAR(Date) = @year 
+                                                    GROUP BY UserId
+                                                    ) AS t 
+                                                    ON t.UserId = u.UserId"
+                                                  , new { month = months, year = year });
+
+                updateoutput(getHours);
+
+
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var months = comboBox1.SelectedItem.ToString();
+            var year = comboBox2.SelectedItem.ToString();
+
+
+            using (var conn = CreateConnection())
+            {
+                var getHours = conn.Query<Projects>(@"SELECT u.Name, t.TotalHoursCaptured FROM Projects AS u
+                                                    JOIN (SELECT ProjectId,SUM(HoursCaptured) AS TotalHoursCaptured FROM Timeslots
+                                                    WHERE DATENAME(month, Date)=@month AND YEAR(Date) = @year 
+                                                    GROUP BY ProjectId
+                                                    ) AS t 
+                                                    ON t.ProjectId = u.ProjectId"
+                                                    , new { month = months, year = year });
+
+
+                updateoutput(getHours);
+            }
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetUsers_Click(object sender, EventArgs e)
+        {
+
+            using (var conn = CreateConnection())
+            {
+                var getString = (@"SELECT * FROM Users
+                                        WHERE Username LIKE @name");
+                updateoutput(conn.Query<Users>(getString, new { name = "%" + usersTxt.Text + "%" }));
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getTimeslots_Click(object sender, EventArgs e)
+        {
+            using (var conn = CreateConnection())
+            {
+                {
+                    var sqlGetTopMatching = (@"SELECT t.* FROM Timeslots AS t
+                                        JOIN (SELECT TOP (1) * FROM Users
+                                        WHERE Username LIKE @name) AS u ON t.UserId=u.UserId
+                                        WHERE DATENAME(month, t.Date)=@month AND YEAR(t.Date) = @year");
+
+                    updateoutput(conn.Query<Timeslots>(sqlGetTopMatching, new { name = "%" + nameTxt.Text + "%", month = comboBox3.SelectedItem.ToString(), year = comboBox4.SelectedItem.ToString()  }));
+                }
+            }
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+           resultTxt.Clear();
+        }
+    }
+
+
+
+
+    public class Users
+    {
+
+        public string Username { get; set; }
+        public float TotalHoursCaptured { get; set; }
+        public override string ToString()
+        {
+            return $"Username: {Username} - Hours: {TotalHoursCaptured}";
+
+        }
+    }
+
+
+    public class Projects
+    {
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+        public float TotalHoursCaptured { get; set; }
+        public override string ToString()
+        {
+            return $"Name:{Name} - Hours:{TotalHoursCaptured}  {"\n\n"}";
+        }
+    }
+
+    public class Timeslots
+    {
+
+
+        public float HoursCaptured { get; set; }
+        public int TimeslotId { get; set; }
+        public int ProjectId { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+        public override string ToString()
+        {
+            return $"Timeslots ID {TimeslotId}- Project ID{ProjectId}- User ID{UserId}-Date{Date}-Hours{HoursCaptured}";
+
+        }
+    }
+}
+
